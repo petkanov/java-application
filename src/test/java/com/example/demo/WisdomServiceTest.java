4dfcc7b4432f9dec5e6b22937d578828a416a693
Peter Petkanov <peter_black@angelic.com>
2024-02-21 23:07:45 -0500

    test annotations

--- src/test/java/com/example/demo/WisdomServiceTest.java
+++ src/test/java/com/example/demo/WisdomServiceTest.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
+@ExtendWith(MockitoExtension.class)
 class WisdomServiceTest {
 
     @Test
