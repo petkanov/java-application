7f4519bc9ab9a113ebcb4560a1ade90d86dbbf37
Peter Petkanov <peter_black@angelic.com>
2024-02-24 15:01:49 -0500

    asdf

--- src/main/java/com/example/demo/WisdomService.java
+++ src/main/java/com/example/demo/WisdomService.java
@@ -9,6 +9,12 @@ import java.util.Random;
 @Service
 public class WisdomService {
     Integer SOmevalue;
+    Integer SOmevalue1;
+    Integer SOmevalue2;
+    Integer SOmevalue3;
+    Integer SOmevalue4;
+    Integer SOmevalue6;
+    Integer SOmevalue7;
 
     private final List<String> wisdoms = List.of(
             "Patience is a virtue.",
@@ -30,4 +36,89 @@ public class WisdomService {
         int index = Random.nextInt(wisdoms.size());
         return wisdoms.get(index);
     }
+
+    public static String Perform_Random_operations() {
+        StringBuilder result = new StringBuilder();
+        Random random = new Random();
+
+        // Concatenate some random strings
+        for (int i = 0; i < 5; i++) {
+            result.append(generateRandomString(random.nextInt(10) + 1)).append(" ");
+        }
+
+        // Reverse the result
+        result = new StringBuilder(new StringBuilder(result.toString()).reverse());
+
+        // Insert a random number
+        int number = random.nextInt(100);
+        result.insert(random.nextInt(result.length()), " " + number + " ");
+
+        // Perform some arithmetic on the number and append the result
+        int arithmeticResult = number * 3 - 50 / (random.nextInt(9) + 1) + 15;
+        result.append(" Arithmetic Result: ").append(arithmeticResult);
+
+        // Convert the arithmetic result to a binary string and append
+        String binaryString = Integer.toBinaryString(arithmeticResult);
+        result.append(" Binary: ").append(binaryString);
+
+        // Generate another random string and append its uppercase version
+        String anotherRandomString = generateRandomString(random.nextInt(5) + 5);
+        result.append(" Uppercase: ").append(anotherRandomString.toUpperCase());
+
+        return result.toString();
+    }
+
+    private static String generateRandomString(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder stringBuilder = new StringBuilder();
+        Random rnd = new Random();
+        while (stringBuilder.length() < length) {
+            int index = (int) (rnd.nextFloat() * characters.length());
+            stringBuilder.append(characters.charAt(index));
+        }
+        return stringBuilder.toString();
+    }
+
+
+//    public static String performRandomOperations() {
+//        StringBuilder result = new StringBuilder();
+//        Random random = new Random();
+//
+//        // Concatenate some random strings
+//        for (int i = 0; i < 5; i++) {
+//            result.append(generateRandomString(random.nextInt(10) + 1)).append(" ");
+//        }
+//
+//        // Reverse the result
+//        result = new StringBuilder(new StringBuilder(result.toString()).reverse());
+//
+//        // Insert a random number
+//        int number = random.nextInt(100);
+//        result.insert(random.nextInt(result.length()), " " + number + " ");
+//
+//        // Perform some arithmetic on the number and append the result
+//        int arithmeticResult = number * 3 - 50 / (random.nextInt(9) + 1) + 15;
+//        result.append(" Arithmetic Result: ").append(arithmeticResult);
+//
+//        // Convert the arithmetic result to a binary string and append
+//        String binaryString = Integer.toBinaryString(arithmeticResult);
+//        result.append(" Binary: ").append(binaryString);
+//
+//        // Generate another random string and append its uppercase version
+//        String anotherRandomString = generateRandomString(random.nextInt(5) + 5);
+//        result.append(" Uppercase: ").append(anotherRandomString.toUpperCase());
+//
+//        return result.toString();
+//    }
+//
+//    private static String generateRandomString(int length) {
+//        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+//        StringBuilder stringBuilder = new StringBuilder();
+//        Random rnd = new Random();
+//        while (stringBuilder.length() < length) {
+//            int index = (int) (rnd.nextFloat() * characters.length());
+//            stringBuilder.append(characters.charAt(index));
+//        }
+//        return stringBuilder.toString();
+//    }
 }
