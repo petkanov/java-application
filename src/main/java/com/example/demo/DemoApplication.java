5594fb8c8e0359be346f197c947ea98d6e0be950
Peter Petkanov <peter_black@angelic.com>
2024-02-24 14:40:33 -0500

    additional mess

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +14,30 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner asd() {
+		return args -> {
+			Asd asd = createMe("first");
+			String asd2 = asd.value("last");
+			System.out.println(asd.value("last"));
+			System.out.println(asd.value("last"));
+			System.out.println(asd.value("last"));
+		};
+	}
+
+	private Asd createMe(String text) {
+		AtomicInteger number = new AtomicInteger(13);
+		return new Asd() {
+
+			@Override
+			public String value(String data) {
+				number.incrementAndGet();
+				return data + " " + text + " = " + number;
+			}
+		};
+	}
+}
+
+interface Asd {
+	String value(String data);
 }
