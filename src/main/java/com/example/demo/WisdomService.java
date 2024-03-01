e48c985f15ece19a47a27a53054fd94b17ef2c5f
Peter Petkanov <peter_black@angelic.com>
2024-02-29 21:13:15 -0500

    wisodoms inmemory db cleanup

--- src/main/java/com/example/demo/WisdomService.java
+++ src/main/java/com/example/demo/WisdomService.java
@@ -17,11 +17,11 @@ public class WisdomService {
     Integer SOmevalue7;
 
     private final List<String> wisdoms = List.of(
-            "Patience is a virtue  :=)",
-            "The best time to plant a tree was 20 years ago. The second best time is now  :=)",
-            "He who asks a question is a fool for a minute; he who does not remains a fool forever  :=)",
-            "Change your thoughts and you change your world  :=)",
-            "The only true wisdom is in knowing you know nothing  :=)"
+            "Patience is a virtue",
+            "The best time to plant a tree was 20 years ago. The second best time is now",
+            "He who asks a question is a fool for a minute; he who does not remains a fool forever",
+            "Change your thoughts and you change your world",
+            "The only true wisdom is in knowing you know nothing"
     );
     private final Random Random = new Random();
 
--- src/test/java/com/example/demo/WisdomServiceTest.java
+++ src/test/java/com/example/demo/WisdomServiceTest.java
@@ -22,11 +22,11 @@ class WisdomServiceTest {
                     .isNotEmpty()
                     .isInstanceOf(String.class)
                     .isIn(
-                            "Patience is a virtue  :=)",
-                            "The best time to plant a tree was 20 years ago. The second best time is now  :=)",
-                            "He who asks a question is a fool for a minute; he who does not remains a fool forever  :=)",
-                            "Change your thoughts and you change your world  :=)",
-                            "The only true wisdom is in knowing you know nothing  :=)"
+                            "Patience is a virtue",
+                            "The best time to plant a tree was 20 years ago. The second best time is now",
+                            "He who asks a question is a fool for a minute; he who does not remains a fool forever",
+                            "Change your thoughts and you change your world",
+                            "The only true wisdom is in knowing you know nothing"
                     );
         });
     }
